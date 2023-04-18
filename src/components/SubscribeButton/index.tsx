46930082d3c452e81ce2193c5238486d49894fe9
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2023-04-18 11:01:23 -0300

    create-api

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { signIn, useSession } from "next-auth/react";
 import styles from "./style.module.scss";
+import { api } from "@/services/api";
+import { getStripeJs } from "@/services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -11,10 +13,22 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
 
   console.log(priceId);
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error);
     }
   }
 
